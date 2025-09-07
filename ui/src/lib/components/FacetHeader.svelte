3ba01d4a77258b175b3f6d372ba92656a78bcd1d
luk-lo <44285808+luk-lo@users.noreply.github.com>
2025-09-07 18:24:26 +0200

    Colored cost icon
    - Make the card cost icon reflect the card civilizations

--- ui/src/lib/components/FacetHeader.svelte
+++ ui/src/lib/components/FacetHeader.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
 	import Civilization from './Civilization.svelte';
 	import { Civ } from '$lib/types/card';
+	import CostIcon from './CostIcon.svelte';
 
 	let { cost, civilizations }: { cost: string | null; civilizations: Array<Civ> } = $props();
 	let hasCost = $state(cost != undefined && cost != null);
 </script>
 
-<div class="flex flex-row" class:justify-end={!hasCost} class:justify-between={hasCost}>
+<div class="flex flex-row text-xl" class:justify-end={!hasCost} class:justify-between={hasCost}>
 	{#if cost}
-		<div
-			class="inline-flex h-8 w-8 items-center justify-center overflow-hidden rounded-full bg-slate-200"
-		>
-			<span class="text-lg font-semibold">{cost}</span>
-		</div>
+		<CostIcon civs={civilizations} {cost}></CostIcon>
 	{/if}
 	<div class=" flex flex-row flex-wrap gap-1">
 		{#each civilizations as civ (civ)}
