80e290f72b21c29150698d02657472992ad83dde
luk-lo <44285808+luk-lo@users.noreply.github.com>
2025-09-13 16:12:02 +0200

    Add guard strike icon

---
+++ ui/src/lib/components/effectIcons/GuardStrike.svelte
@@ -0,0 +1,17 @@
+<span class="inline-flex h-[1.2em] w-[1.2em] items-center justify-center">
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		xml:space="preserve"
+		title="guard strike icon"
+		viewBox="0 0 256 256"
+		><g style="display:inline"
+			><path
+				d="M106 142 78 92l2 26-57-9 28 35-52 37 59 1-16 65 55-46h6l9 50 18-50h7l51 44-28-64 2-4 45-3-61-15-58 25-4-4z"
+				style="display:inline;fill:#000;fill-opacity:.997409;stroke-width:5.70652"
+			/><path
+				d="m174 17-48 64 23 9-29 31 20 9-29 32 101-44-28-12 39-28-22-9 47-38z"
+				style="opacity:1;fill:#fefe03;fill-opacity:1;fill-rule:nonzero;stroke:#000;stroke-width:5.70652;stroke-dasharray:none;stroke-opacity:1"
+			/></g
+		></svg
+	>
+</span>
--- ui/src/lib/components/effectIcons/Strikeback.svelte
+++ ui/src/lib/components/effectIcons/Strikeback.svelte
@@ -1,5 +1,11 @@
 <span class="mx-1 inline-flex h-[1.2em] w-[1.2em] items-center justify-center">
-	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" class="inline" style="color: #000">
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 256 256"
+		class="inline"
+		title="strike back icon"
+		style="color: #000"
+	>
 		<path
 			d="M70 3 1 161 49 145 40 253 219 173 180 145v-7l76-80-142 82 32 26-53 18 37-110-58 21z"
 			fill="currentColor"
