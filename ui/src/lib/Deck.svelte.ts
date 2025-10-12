77e13dfd57e5f42c94c78cfc7c632fb8a039b153
luk-lo <44285808+luk-lo@users.noreply.github.com>
2025-10-12 11:19:51 +0200

    Format

--- ui/src/lib/Deck.svelte.ts
+++ ui/src/lib/Deck.svelte.ts
@@ -22,14 +22,14 @@ export class Deck {
 	}
 
 	async loadCollection(fetch: (input: RequestInfo | URL, init?: RequestInit) => Promise<Response>) {
-			const response = await fetch(`/api/deck/${this.id}`);
-			if (response.ok) {
-				this.collection = (await response.json()) as CollectionData;
-			} else if (response.status === 401 || response.status === 403) {
-				throw new Error('unauthorized');
-			} else {
-				console.error(response.statusText);
-			}
+		const response = await fetch(`/api/deck/${this.id}`);
+		if (response.ok) {
+			this.collection = (await response.json()) as CollectionData;
+		} else if (response.status === 401 || response.status === 403) {
+			throw new Error('unauthorized');
+		} else {
+			console.error(response.statusText);
+		}
 	}
 
 	async getAmount(cardId: number): Promise<number> {
@@ -38,40 +38,40 @@ export class Deck {
 
 	async setCardAmount(cardId: number, amount: number) {
 		if (this.collection) {
-				const response = await fetch(`/api/deck/${this.collection.info.id}/cards/${cardId}`, {
-					method: 'PUT',
-					headers: {
-						'Content-Type': 'application/json',
-						'X-XSRF-TOKEN':
-							document.cookie
-								.split('; ')
-								.find((row) => row.startsWith('XSRF-TOKEN='))
-								?.split('=')[1] ?? ''
-					},
-					body: JSON.stringify({ amount: amount })
-				});
-				if (response.ok) {
-					const updatedDeckInfo = (await response.json()) as CollectionInfo;
-					if (!this.collection) {
-						await this.loadCollection(fetch);
-					} else {
-						this.collection.info = updatedDeckInfo;
-						const idx = this.collection.cardPage.content.findIndex((c) => c.id == cardId);
-						if (idx >= 0) {
-							if (amount == 0) {
-								this.collection.cardPage.content.splice(idx, 1);
-							} else {
-								this.collection.cardPage.content[idx].amount = amount;
-							}
-						} else {
-							await this.loadCollection(fetch);
-						}
-					}
-				} else if (response.status === 401 || response.status === 403) {
-					throw new Error('unauthorized');
+			const response = await fetch(`/api/deck/${this.collection.info.id}/cards/${cardId}`, {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+					'X-XSRF-TOKEN':
+						document.cookie
+							.split('; ')
+							.find((row) => row.startsWith('XSRF-TOKEN='))
+							?.split('=')[1] ?? ''
+				},
+				body: JSON.stringify({ amount: amount })
+			});
+			if (response.ok) {
+				const updatedDeckInfo = (await response.json()) as CollectionInfo;
+				if (!this.collection) {
+					await this.loadCollection(fetch);
 				} else {
-					console.log(response.statusText);
+					this.collection.info = updatedDeckInfo;
+					const idx = this.collection.cardPage.content.findIndex((c) => c.id == cardId);
+					if (idx >= 0) {
+						if (amount == 0) {
+							this.collection.cardPage.content.splice(idx, 1);
+						} else {
+							this.collection.cardPage.content[idx].amount = amount;
+						}
+					} else {
+						await this.loadCollection(fetch);
+					}
 				}
+			} else if (response.status === 401 || response.status === 403) {
+				throw new Error('unauthorized');
+			} else {
+				console.log(response.statusText);
+			}
 		}
 	}
 }
