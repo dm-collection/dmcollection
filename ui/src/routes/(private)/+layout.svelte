f20289f9fca5ffac61233ad4ada08cff8394d718
luk-lo <44285808+luk-lo@users.noreply.github.com>
2025-11-02 14:11:20 +0100

    Improve (private) base layout

--- ui/src/routes/(private)/+layout.svelte
+++ ui/src/routes/(private)/+layout.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import { authState } from '$lib/auth.svelte';
 	import NavigationMenu from '$lib/components/NavigationMenu.svelte';
 	let { children } = $props();
+	let noScroll = $derived(page.url.pathname.includes('/deck/'));
 </script>
 
 <svelte:head>
 	<title>DM Collection</title>
 </svelte:head>
 
-<header>
-	<NavigationMenu enabled={true} username={authState.username} />
-</header>
-<main class="sticky m-8 mb-2">
-	{@render children()}
-</main>
+<div class={['flex', !noScroll && 'h-screen', 'flex-col', noScroll && 'overflow-hidden']}>
+	<header class="sticky top-0 z-20 min-h-0 shrink-0">
+		<NavigationMenu enabled={true} username={authState.username} />
+	</header>
+	<main class={['flex-1 p-3 md:p-8', noScroll ? 'overflow-hidden': 'overflow-y-auto ']}>
+		{@render children()}
+	</main>
+</div>
