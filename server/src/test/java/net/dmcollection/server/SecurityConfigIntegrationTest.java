3d97db8213be60de3177c8c3d3d48811ff018cfc
luk-lo <44285808+luk-lo@users.noreply.github.com>
2026-05-02 20:08:35 +0200

    Fix test
    Remove no longer public url

--- server/src/test/java/net/dmcollection/server/SecurityConfigIntegrationTest.java
+++ server/src/test/java/net/dmcollection/server/SecurityConfigIntegrationTest.java
@@ -1,10 +1,5 @@
 package net.dmcollection.server;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -12,21 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @AutoConfigureMockMvc
 class SecurityConfigIntegrationTest extends IntegrationTestBase {
 
   @Autowired private MockMvc mockMvc;
 
   @ParameterizedTest
-  @ValueSource(
-      strings = {
-        "/",
-        "/api/auth/status",
-        "/index.html",
-        "/favicon.ico",
-        "/favicon.png",
-        "/_app/env.js"
-      })
+  @ValueSource(strings = {"/", "/index.html", "/favicon.ico", "/favicon.png", "/_app/env.js"})
   void publicPathsGet(String path) throws Exception {
     mockMvc.perform(get(path)).andExpect(status().isOk());
   }
